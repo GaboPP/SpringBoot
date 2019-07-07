1674e807a0827cb6b3d390c814a8dc6afcffec14
Jean Gonzalez <jean.gonzaleza@sansano.usm.cl>
2019-07-07 16:54:02 -0400

    NEW View of Projects

--- server/src/main/java/com/isw/server/models/MemberProyectId.java
+++ server/src/main/java/com/isw/server/models/MemberProyectId.java
@@ -6,8 +6,24 @@ public class MemberProyectId implements Serializable{
     private int id_proyecto;
     private int id_participante;
 
-    public MemberProyectId(int id_proyecto, int id_participante){
-        this.id_participante = id_participante;
+    public MemberProyectId(int id_proyecto, int id_participante) {
+        this.setId_participante(id_participante);
+        this.setId_proyecto(id_proyecto);
+    }
+
+    public int getId_proyecto() {
+        return id_proyecto;
+    }
+
+    public void setId_proyecto(int id_proyecto) {
         this.id_proyecto = id_proyecto;
+    }
+
+    public int getId_participante() {
+        return id_participante;
+    }
+
+    public void setId_participante(int id_participante) {
+        this.id_participante = id_participante;
     }
 }
