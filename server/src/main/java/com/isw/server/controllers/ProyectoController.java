7daf2f80ac771a133c18a37105cfff7d8a97525e
Jean Gonzalez <jean.gonzaleza@sansano.usm.cl>
2019-07-09 11:11:22 -0400

    Matching con gabriel

--- server/src/main/java/com/isw/server/controllers/ProyectoController.java
+++ server/src/main/java/com/isw/server/controllers/ProyectoController.java
@@ -1,14 +1,19 @@
 package com.isw.server.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.isw.server.models.proyecto;
 import com.isw.server.repository.ProyectoRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.isw.server.models.proyecto;
-import java.util.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/proyectos")
@@ -22,5 +27,26 @@ public class ProyectoController{
         List<proyecto> proyectos = proyectoRepo.findAll();
         model.addAttribute("proyectos", proyectos);
         return "proyectos";
+    }
+
+    @GetMapping(value = "/getProyect")
+    public List<proyecto> getProyect(){
+        return proyectoRepo.findAll();
+    }
+
+    @GetMapping("/proyect_detail/{id}")
+    public ModelAndView getproyect_detail(@PathVariable(required = true, name = "id") int id
+    ){        
+        ModelAndView modelAndView = new ModelAndView("proyect_details/proyect_detail");
+        List<proyecto> proyectos = this.getProyect().stream().filter( (p) -> { return p.getId_proyecto() == id;}).collect(Collectors.toList());
+        modelAndView.addObject("post", proyectos.get(0));
+        return modelAndView.addObject("proyecto", new proyecto());
+    }
+    @PostMapping("/add_proyect_detail")
+    public String add_proyect_detail(proyecto post_evaluation, Model model) {
+        List<proyecto> posts = this.getProyect();
+        posts.add(post_evaluation);
+        model.addAttribute("posts", posts);
+        return "index";
     }
 }
