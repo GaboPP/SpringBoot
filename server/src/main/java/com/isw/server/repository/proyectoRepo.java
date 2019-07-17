d035253295f2b09da29c4b7dded8da7bb7530d51
GaboPP <gabriel.astorga@sansano.usm.cl>
2019-07-17 00:08:29 -0400

    121

--- server/src/main/java/com/isw/server/repository/proyectoRepo.java
+++ server/src/main/java/com/isw/server/repository/proyectoRepo.java
@@ -3,6 +3,6 @@ package com.isw.server.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.isw.server.models.proyecto;
 
-public interface proyectoRepo extends JpaRepository<proyecto, Integer> { //<modelo,llave primaria>
+public interface ProyectoRepo extends JpaRepository<proyecto, Integer> {
 
 }
